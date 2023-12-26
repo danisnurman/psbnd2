23dd712e5bb095080f119f38517ed1391aaeb4ee
Danis Nurmansyah <danisnurman@users.noreply.github.com>
2023-12-26 16:57:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -17,11 +17,11 @@ st.title('Diabetes Health Indicators')
 st.write("Hi! Please fill the form below.")
 
 ###
-df = pd.read_csv('https://raw.githubusercontent.com/danisnurman/psbnd2/main/diabetes_binary_5050split_health_indicators_BRFSS2015.csv')
+df = pd.read_csv('/workspaces/psbnd2/diabetes_binary_5050split_health_indicators_BRFSS2015.csv')
 # st.dataframe(df, use_container_width=True)
 df.dropna(inplace=True)
 df.isnull().sum()
-feature_cols = ['Diabetes_binary', 'HighBP', 'HighChol', 'CholCheck', 'BMI', 'Smoker', 'Stroke', 'HeartDiseaseorAttack']
+feature_cols = ['Diabetes_binary', 'HighBP', 'HighChol', 'BMI', 'Smoker', 'PhysActivity']
 df = df[feature_cols]
 st.write(feature_cols)
 X = df.drop(columns='Diabetes_binary')
@@ -43,53 +43,31 @@ clf = clf.fit(X_train, y_train)
 y_pred = clf.predict(X_test)
 st.write("Accuracy: ", metrics.accuracy_score(y_test, y_pred))
 
-predict_result = clf.predict([[0,0,1,53,0,0,0]])
+predict_result = clf.predict([[0,0,53,0,0]])
 st.write(predict_result)
-
-### Visualize
-# from sklearn.tree import export_graphviz
-# from six import StringIO
-# from IPython.display import Image
-# import pydotplus
-
-# dot_data = StringIO()
-# export_graphviz(clf, out_file=dot_data,
-#                 filled=True, rounded=True,
-#                 special_characters=True, feature_names=
-#                 ['bp', 'chol', 'cholcheck', 'bmi', 'smoker', 'stroke', 'heart'],
-#                 class_names=['0', '1'])
-# graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
-# graph.write_png('diabetes.png')
-# Image(graph.create_png())
-###
-
-###
-# df2 = pd.DataFrame(
-#     [
-#         {"command": "st.selectbox", "rating": 4, "is_widget": True},
-#         {"command": "st.balloons", "rating": 5, "is_widget": False},
-#         {"command": "st.time_input", "rating": 3, "is_widget": True},
-#     ]
-# )
-# st.dataframe(df2, use_container_width=True)
-###
 
 ###
 bpVal = st.number_input(label="High BP?", min_value=0, max_value=1)
-cholVal = st.number_input(label="High Cholesterol?", min_value=0, max_value=1)
-cholCheckVal = st.number_input(label="Cholesterol Check?", min_value=0, max_value=1)
+cholVal = st.number_input(label="Cholesterol Total", min_value=10, max_value=500)
 bmiVal = st.number_input(label="Body Mass Index", min_value=10, max_value=100)
 smokerVal = st.number_input(label="Smoker?", min_value=0, max_value=1)
-strokeVal = st.number_input(label="Stroke?", min_value=0, max_value=1)
-heartVal = st.number_input(label="Heart Disease or Attack?", min_value=0, max_value=1)
+physActVal = st.number_input(label="Physical Activity?", min_value=0, max_value=1)
 ###
 
-st.write(bpVal, cholVal, cholCheckVal, bmiVal, smokerVal, strokeVal, heartVal)
+st.write(bpVal, cholVal, bmiVal, smokerVal, physActVal)
+cholStatus = 0
 
-predict_result = clf.predict([[bpVal, cholVal, cholCheckVal, bmiVal, smokerVal, strokeVal, heartVal]])
+if(cholVal>=10 and cholVal<=200):
+    cholStatus = 0
+elif(cholVal>200):
+    cholStatus = 1
+
+st.write("Cholesterol status: ", cholStatus)
+
+predict_result = clf.predict([[bpVal, cholStatus, bmiVal, smokerVal, physActVal]])
 if(predict_result==0):
-    st.write(predict_result, "Not Risk")
+    st.write("Diabetes status: Not Risk")
 else:
-    st.write(predict_result, "Risk!")
+    st.write("Diabetes status: Risk!")
 # st.write(predict_result)
 ###
